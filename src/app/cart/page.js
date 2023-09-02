c33eb81eb5bd25be9e0ccf6e8dbedaac7e663811
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-09-02 16:40:28 +0530

    Cart Page completed

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,17 +1,75 @@
 "use client"
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { removeItem } from '../../../redux/slices/cartSlice';
 import Image from 'next/image';
-import { FaMinus, FaPlus } from 'react-icons/fa';
+// import { FaMinus, FaPlus } from 'react-icons/fa';
+import { BiSolidChevronLeft } from 'react-icons/bi'
+import { ImBin } from 'react-icons/im'
+import Lottie from 'lottie-react';
+import Empty from '../../../components/Animation/EmptyCart.json';
 
 const page = () => {
   const dispatch = useDispatch();
-    const items = useSelector((state) => state.cart);
-    console.log("CART ITEMS: ", items);
+  const items = useSelector((state) => state.cart);
+  const total = items.reduce((a, b) => a + b.price, 0);
+  console.log("CART ITEMS: ", items);
 
   return (
-    <section>
+    <section className="contaner mx-auto lg:px-20 md:px-3 px-3 my-10">
+      <h1 className="text-5xl capitalize font-bold lg:text-left md:text-center text-center mb-10">{items.length !== 0 ? ('items in cart') : ('no items in the cart')}</h1>
+
+      <div className="lg:w-[90vw] md:w-[90vw]  flex flex-col justify-center items-center">
+
+        {/* ITEMS IN CART */}
+        <div>
+
+          {/* RENDERING ITEMS */}
+          <div className="lg:w-[90vw] md:w-[90vw]">
+
+            {items.length !== 0 ? (items.map((item) => (
+              <div key={item.id} className="border-b-2 border-[#6D6D6D] flex justify-between items-center pb-2 mb-10">
+                <div>
+                  <Image src={item.img} alt={item.name} className="h-[100px] w-[200px]" />
+                  <h1 className='text-2xl font-bold mt-2'>{item.name}</h1>
+                </div>
+                {/* <div className="flex justify-center items-center gap-2 text-xl rounded-full border-2 border-[#6D6D6D] px-2 py-1">
+                  <FaMinus />
+                  <span className="text-3xl text-[#FF6452]">1</span>
+                  <FaPlus />
+                </div> */}
+                <div className="flex lg:flex-row md:flex-row flex-col justify-between items-center lg:gap-0 md:gap-20 gap-10 lg:w-[500px] md:w-[250px]">
+                  <h1 className="text-xl font-bold text-[#6D6D6D]">${item.price.toFixed(2)}</h1>
+                  <ImBin className='text-2xl text-red-600 cursor-pointer' onClick={e => dispatch(removeItem())} />
+                </div>
+              </div>
+            ))) : (
+              <div className="w-[90vw] flex items-center justify-center lg:mb-0 md:mb-0 mb-10">
+                <div>
+                  <Lottie animationData={Empty} loop={true} className="lg:h-[500px] lg:w-[500px] md:h-
+                        [350px] md:w-[350px] h-[300px] w-[300px]"
+                  />
+                </div>
+              </div>)}
+          </div>
+
+
+          {/* TOTAL SUMMARY */}
+          <div className='flex lg:flex-row md:flex-col-reverse flex-col-reverse lg:gap-0 md:gap-10 gap-10 lg:justify-between md:justify-center justify-center lg:items-end md:items-center items-center'>
+            <div>
+              <button className="flex justify-center items-center gap-2 rounded-full bg-[#FF6452] text-white px-7 py-4 text-xl w-auto capitalize font-bold"><span className="bg-white text-[#FF6452] rounded-full h-5 w-5"><BiSolidChevronLeft /></span>continue shopping</button>
+            </div>
+
+            {/* TOTAL AMOUNT */}
+            <div className="bg-[#F0F0F0] flex flex-col justify-center items-center p-4 w-[300px]">
+              <h1 className='text-2xl font-bold capitalize flex justify-between w-full'>subtotal: <span className='font-medium'>${items.length === 0 ? ('0') : (total.toFixed(2))}</span></h1>
+              <h1 className='text-2xl font-bold capitalize flex justify-between w-full'>shipping: <span className='font-medium'>$50.00</span></h1>
+              <div className="w-full h-[2px] bg-gray-500 my-5"></div>
+              <h1 className='text-2xl font-bold capitalize flex justify-between w-full text-[#FF6452]'>total: <span className='font-medium'>${items.length === 0 ? ('0') : ((total+50.00).toFixed(2))}</span></h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/app/products/data.js
+++ src/app/products/data.js
@@ -19,105 +19,105 @@ export const data = [
     {
         id: 1,
         name:"Nike Air Jordan-01",
-        price:"200.20",
+        price:200.20,
         rating: 4.5,
         img:shoe1
     },
     {
         id: 2,
         name:"Nike Air Jordan-10",
-        price:"210.20",
+        price:210.20,
         rating: 4.7,
         img:shoe2
     },
     {
         id: 3,
         name:"Nike Air Jordan-100",
-        price:"220.20",
+        price:220.20,
         rating: 4.9,
         img:shoe3
     },
     {
         id: 4,
         name:"Nike Adapt BB 2.0",
-        price:"200.00",
+        price:200.00,
         rating: 4.1,
         img:shoe4
     },
     {
         id: 5,
         name:"Nike Martine Rose",
-        price:"250.20",
+        price:250.20,
         rating: 4.3,
         img:shoe5
     },
     {
         id: 6,
         name:"Nike Smart Shoe 2.0",
-        price:"205.20",
+        price:205.20,
         rating: 5.0,
         img:shoe6
     },
     {
         id: 7,
         name:"Nike Air Low Premium",
-        price:"150.00",
+        price:150.00,
         rating: 3.9,
         img:shoe7
     },
     {
         id: 8,
         name:"Nike Air Force",
-        price:"180.20",
+        price:180.20,
         rating: 4.1,
         img:shoe8
     },
     {
         id: 9,
         name:"Nike Adapt BB Rose",
-        price:"200.20",
+        price:200.20,
         rating: 4.9,
         img:shoe9
     },
     {
         id: 10,
         name:"Nike Air Premium",
-        price:"240.00",
+        price:240.00,
         rating: 4.9,
         img:shoe10
     },
     {
         id: 11,
         name:"Nike Adapt BB Pro",
-        price:"225.20",
+        price:225.20,
         rating: 4.5,
         img:shoe11
     },
     {
         id: 12,
         name:"Air Jordan PR3",
-        price:"120.00",
+        price:120.00,
         rating: 4.1,
         img:shoe12
     },
     {
         id: 13,
         name:"Nike Multi Smart Shoe",
-        price:"150.00",
+        price:150.00,
         rating: 4.0,
         img:shoe13
     },
     {
         id: 14,
         name:"Nike Air Black Max",
-        price:"200.00",
+        price:200.00,
         rating: 5.0,
         img:shoe14
     },
     {
         id: 15,
         name:"Nike Zoom Max",
-        price:"300.00",
+        price:300.00,
         rating: 4.2,
         img:shoe15
     },
